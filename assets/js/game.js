e9b14c6e43320e3f814041dba01991e15af82008
Shamim Imtiaz <75001492+shamimimtiaz@users.noreply.github.com>
2020-12-15 01:42:36 -0500

    function

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,1 +1,12 @@
-window.alert("This is an alert! JavaScript is running!");
+//window.alert("This is an alert! JavaScript is running!");//
+
+var playerName = window.prompt("What is your robot's name?");
+//Note the lack of quotation marks around player name
+console.log(playerName);
+
+function fight() {
+    window.alert("The fight has begun");
+
+fight();
+
+
