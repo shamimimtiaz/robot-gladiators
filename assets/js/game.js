82675bc191f9ef22bc29bfeedb37b0cf36d77111
Shamim Imtiaz <75001492+shamimimtiaz@users.noreply.github.com>
2020-12-16 12:30:30 -0500

    add enemy combatants

--- assets/js/game.js
+++ assets/js/game.js
@@ -6,12 +6,18 @@ var playerMoney = 10;
 // You can also log multiple values at once like this
 console.log(playerName, playerAttack, playerHealth);
 
-var enemyName = "Roborto";
+var enemyNames = ['Roborto', 'Amy Android', 'Robo Trumble'];
 var enemyHealth = 50;
 var enemyAttack = 12;
 
+console.log(enemyNames);
+console.log(enemyNames.length);
+console.log(enemyNames[0]);
+console.log(enemyNames[3]);
+
 var fight = function() {
     // Alert players that they are starting the round
+
   window.alert("Welcome to Robot Gladiators!");
   
   var promptFight = window.prompt ("Would you like to FIGHT or SKIP this battle? Enter 'FIGHT' or 'SKIP' to choose."); 
@@ -19,11 +25,12 @@ var fight = function() {
 // if player choses to fight, then fight
 if (promptFight === "FIGHT" || promptFight === "fight") {
     
-
 // remove enemy's health by subtracting the amount set in the playerAttack variable
 //Subtract the value of `playerAttack` from the value of `enemyHealth` and use that result to update the value in the `enemyHealth` variable
+
 enemyHealth = enemyHealth-playerAttack;
 // Log a resulting message to the console so we know that it worked.
+
 console.log(playerName + " attacked " + enemyName + ". " + enemyName + " now has " + enemyHealth + " health remaining.");
 
   //check enemy's health
@@ -36,6 +43,7 @@ console.log(playerName + " attacked " + enemyName + ". " + enemyName + " now has
 
   // remove player's health by subtracting the amount set in the enemyAttack variable
   // Subtract the value of `enemyAttack` from the value of `playerHealth` and use that result to update the value in the `playerHealth` variable.
+
   playerHealth = playerHealth - enemyAttack;
   
   // Log a resulting message to the console so we know that it worked.
@@ -71,4 +79,13 @@ console.log(playerName + " attacked " + enemyName + ". " + enemyName + " now has
 };
 
 // run fight function to start game
-fight();
+//fight();
+
+var enemyNames ["rob", "Amy", "Robo"];
+console.log(enemyNames);
+
+for (var i=0; i< enemyNames.lenght; i++);
+console.log(enemyNames[i]);
+console.log(i);
+console.log(enemyName[i] + "is at" + i + "index");
+}
